e0ed431734d3238648863391b8fbccdb7c44edaf
Hugues Martin <hugues.martin@u-psud.fr>
2024-02-06 16:00:19 +0100

    Testing Human function and adding some feature to the menu

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -7,15 +7,24 @@ using UnityEngine.UI;
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] Slider volumeSlider;
+    private string sliderPrefKey = "SliderValue";
+
+    public void playSound(AudioSource asa)
+    {
+        asa.Play();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        volumeSlider.value = PlayerPrefs.GetFloat(sliderPrefKey, 0.5f);
+        AudioListener.volume = volumeSlider.value;
     }
 
     public void ChangeVolume()
     {
         AudioListener.volume = volumeSlider.value;
+        PlayerPrefs.SetFloat(sliderPrefKey, volumeSlider.value);
+        PlayerPrefs.Save();
     }
 }
