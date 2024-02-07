1dbe0d7a5392e78ea23e8ae67e48e9b42ddd5e56
Esteban <esteban@neraudau.fr>
2024-02-07 13:14:02 +0100

    Add some prefab and update secret Room

--- Assets/Scripts/PnjSoundManager.cs
+++ Assets/Scripts/PnjSoundManager.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PnjSoundManager : MonoBehaviour
 {
+    public Console console;
     public void playSound(AudioSource audio)
     {
+	console.AddLine("\nVous avez parlé au guide")
         audio.Play();
     }
 }
