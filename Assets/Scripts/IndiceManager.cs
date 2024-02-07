82586adc4203f4c36145b73a459de42df344cc83
Esteban <esteban@neraudau.fr>
2024-02-07 14:19:57 +0100

    Add debug lines to IndiceManager.cs

--- Assets/Scripts/IndiceManager.cs
+++ Assets/Scripts/IndiceManager.cs
@@ -6,11 +6,13 @@ public class IndiceManager : MonoBehaviour
     [SerializeField] private TMP_Text indicesText; // Référence au composant TextMeshPro qui affiche les indices
     [SerializeField] private string[] indices; // Tableau des indices à afficher
     private int currentIndiceIndex = 0; // L'indice actuel dans le tableau
+    public Console console;
 
     public void ShowNextIndice()
     {
         if (currentIndiceIndex < indices.Length)
         {
+	    console.AddLine("\nL'indice numéro" + currentIndiceIndex + " a été demandé");
             // Afficher l'indice actuel
             indicesText.text = "\n" + indices[currentIndiceIndex];
             // Passer au prochain indice pour la prochaine fois que le bouton est pressé
@@ -26,6 +28,7 @@ public class IndiceManager : MonoBehaviour
     // Optionnel : Méthode pour réinitialiser les indices
     public void ResetIndices()
     {
+	console.AddLine("\nReset des indices");
         currentIndiceIndex = 0;
         indicesText.text = string.Empty;
     }
